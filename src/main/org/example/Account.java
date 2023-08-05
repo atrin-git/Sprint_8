efb595c44bd9dbb0b798bf426f949c9e3557250f
Aleksandra Sergeeva <al.vi.sergeeva@yandex.ru>
2023-08-05 13:43:58 +0300

    Init Task

---
+++ src/main/org/example/Account.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        /*
+             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+         */
+        return true;
+    }
+
+}
